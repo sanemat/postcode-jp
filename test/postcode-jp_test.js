b7dbfc4c2377a7515e6478ab57d3dcc28e157c6b
sanemat <o.gata.ken@gmail.com>
2014-05-31 18:35:53 +0900

    Add implementation

--- test/postcode-jp_test.js
+++ test/postcode-jp_test.js
@@ -3,10 +3,21 @@
 var postcodeJp = require('../lib/postcode-jp.js');
 var assert = require('power-assert');
 
+describe('fake implementation', function(){
+  var postcode;
 
+  before(function(done){
+    postcode = postcodeJp({ postcode: '060-0041' });
+    done();
+  });
 
-describe('awesome', function(){
-  it('should be awesome', function(){
-    assert.equal(postcodeJp.awesome(), 'awesome');
+  it('should return address', function(done){
+    postcode.addresses(function(addresses){
+      var address = addresses[0];
+      assert.equal(address.pref, '北海道');
+      assert.equal(address.region, '札幌市中央区');
+      assert.equal(address.town, '大通東');
+      done();
+    });
   });
 });
