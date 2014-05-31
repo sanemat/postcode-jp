43036870724f247f78c7785a952447f4f21620f1
sanemat <o.gata.ken@gmail.com>
2014-05-31 18:29:50 +0900

    Update example

--- test/postcode-jp_test.js
+++ test/postcode-jp_test.js
@@ -3,6 +3,8 @@
 var postcodeJp = require('../lib/postcode-jp.js');
 var assert = require('power-assert');
 
+
+
 describe('awesome', function(){
   it('should be awesome', function(){
     assert.equal(postcodeJp.awesome(), 'awesome');
